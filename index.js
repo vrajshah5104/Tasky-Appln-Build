e7c48ab5833274defb2de2bbd6a393783f9b64c7
Vraj <vrajnshah05@gmail.com>
2023-03-20 12:04:23 +0530

    Added JS code for rendering display card and submitting card values

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Inspect element karine console na badle application par jao toh key and value made je store thay eni
 // It's a local storage jene apde set, get and delete kari shakiye and it has less response time
 
+// Kindoff backup storage array
 const state = {
     taskList: []
 }
@@ -13,7 +14,7 @@ const taskModal = document.querySelector(".task__modal__body");
 
 // JS and HTML code for template for the card on screen
 // id, key, ${} can be used to access JS into HTML
-const HtmlTaskContent = ({id, title, description, type, url}) => `
+const htmlTaskContent = ({id, title, description, type, url}) => `
     <div class='col-md-6 col-lg-4 mt-3'id=${id}>
         <div class="card shadow task__card">
             <div class="card-header d-flex justify-content-end task__card__header">
@@ -72,8 +73,31 @@ const htmlModalContent = ({id, title, description, url}) => {
 const updateLocalStorage = () => {
     localStorage.setItem(
         "task",
-        JSON.stringify ({ // This 'stringify' converts json to a string format
+        JSON.stringify ({ // This 'stringify' converts JSON to a string format for local storage
             task: state.taskList
         })
     )
+}
+
+// Loading/Rendering initial data/cards for display on the screen
+const loadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task); // Converting string back to JSON format
+
+    if (localStorageCopy) {
+        state.taskList = localStorageCopy.task;
+    }
+    state.taskList.map ((cardDate) => {
+        taskContents.innerAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+    })
+}
+
+// JS code for submitting or updating the cards values
+const handleSubmit = (event) => {
+    const id = `${date.now()}`;
+    const input = {
+        url: document.getElementById("imageUrl").value,
+        title: document.getElementById("taskTitle").value,
+        type: document.getElementById("taskType").value,
+        description: document.getElementById("taskDescription").value
+    }
 }
