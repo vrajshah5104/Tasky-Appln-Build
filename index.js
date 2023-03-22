832e6f1dc99392636ad54a62dc3ef3b496f69561
Vraj <vrajnshah05@gmail.com>
2023-03-22 17:04:09 +0530

    Added JS codes for delete, edit, save changes and search button

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
     <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
         <div class="card shadow task__card">
             <div class="card-header d-flex justify-content-end task__card__header">
-                <button type="button" class="btn btn-outline-primary mr-1.5" name=${id}>
+                <button type="button" class="btn btn-outline-primary mr-1.5"
+                        onclick="editTask.apply(this, arguments)" name=${id}>
                     <i class="fa-solid fa-pen-fancy" name=${id}></i>
                 </button>
-                <button type="button" class="btn btn-outline-danger mr-1.5" name=${id}>
-                    <i class="fa-solid fa-trash-can" name=${id}></i>
+                <button type="button" class="btn btn-outline-danger mr-1.5"
+                        onclick="deleteTask.apply(this, arguments)" name=${id}>
+                    <i class="fa-solid fa-trash-can" name=${id}"></i>
                 </button>
             </div>
             <div class="class-body">
@@ -39,7 +41,7 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
             </div>
             <div class="card-footer">
                 <button type="button" class="btn btn-outline-info float-right" data-bs-toggle="modal"
-                        data-bs-target="#showTask">Open Task</button>
+                        data-bs-target="#showTask" onclick="openTask()" id=${id}>Open Task</button>
             </div>
         </div>
     </div>
@@ -85,7 +87,7 @@ const loadInitialData = () => {
 }
 
 // JS code for submitting or updating the cards values
-const handleSubmit = (event) => {
+const handleSubmit = (event1) => {
     const id = `${Date.now()}`;
     const input = {
         url: document.getElementById("imageUrl").value,
@@ -93,9 +95,9 @@ const handleSubmit = (event) => {
         type: document.getElementById("taskType").value,
         description: document.getElementById("taskDescription").value,
     };
-    // if (input.title=="" || input.tags=="" || input.description=="") {
-    //     return alert("Please cum inside all the necessary fields ;-)");
-    // }
+    if (input.title=="" || input.tags=="" || input.description=="") {
+        return alert("Please cum inside all the necessary fields ;-)");
+    }
     taskContents.insertAdjacentHTML (
         "beforeend", htmlTaskContent({...input, id})
     );
@@ -126,3 +128,137 @@ const handleSubmit = (event) => {
     console.log({...obj, age: 69});
     {name: 'Vraj', age: 69}
 */
+
+// JS code for when you click on the open task button
+// In 'htmlTaskContent', in the 'openTask' button tag, you can do onclick='openTask.apply(this, arguments)' to get the same results
+const openTask = (event2) => {
+    if (!event2) {
+        event2 = window.event;
+    }
+    const getTask = state.taskList.find(({id}) => id === event2.target.id);
+    taskModal.innerHTML = htmlModalContent(getTask);
+}
+
+// JS code for when you click on the delete button
+const deleteTask = (event3) => {
+    if (!event3) {
+        event3 = window.event; // This means we are getting the event triggered in new window
+    }
+    const targetId = event3.target.getAttribute("name");
+    // console.log(targetId); // Can do this to check the id of the trash/delete buttons
+    const type = event3.target.tagName;
+    // console.log(type); // Can do this to check if you are clicking on an icon or a button
+    const removeTask = state.taskList.filter(({id}) => targetId);
+    updateLocalStorage();
+
+    // Removing the child from UI
+    /* 'Line-25' ma 'onclick' lakhelu che ema jo 'button' par click karo toh badha 'div' ma thi bahar jata
+       4 parents ave, soo e karine oldest parent no child removed...else icon par click karo toh bahar jata
+       5 parents ave soo that */
+    if (type === "BUTTON") {
+        return event3.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+            event3.target.parentNode.parentNode.parentNode
+        )
+    }
+    else if (type === "I") {
+        return event3.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+            e.target.parentNode.parentNode.parentNode.parentNode
+        )
+    }
+}
+
+// JS code for when you click on the edit button
+const editTask = (event4) => {
+    if (!event4) {
+        event4 = window.event;
+    }
+    const targetId = e.target.id;
+    const type = e.target.tagName;
+
+    let parentNode, taskTitle, taskDescription, taskType, submitButton;
+
+    // Logic for 'Line-22'
+    if (type === "BUTTON") {
+        parentNode = e.target.parentNode.parentNode;
+    }
+    else {
+        parentNode = e.target.parentNode.parentNode.parentNode;
+    }
+
+    taskTitle = parentNode.childNodes[3].childNodes[3];
+    taskDescription = parentNode.childNodes[3].childNodes[5];
+    taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    submitButton = parentNode.childNodes[5].childNodes[1];
+
+    taskTitle.setAttribute("contentEditable", "true");
+    taskDescription.setAttribute("contentEditable", "true");
+    taskType.setAttribute("contentEditable", "true");
+
+    submitButton.setAttribute("onclick", "saveEdit.apply(this, arguments)");
+    submitButton.setAttribute("data-bs-toggle");
+    submitButton.setAttribute("data-bs-target");
+    submitButton.innerHTML = "Save Changes";
+}
+
+// JS code to save the changes after editing a task
+const saveEdit = (event5) => {
+    if (!event5) {
+        event5 = window.event;
+    }
+    const targetId = event5.target.id;
+    const parentNode = event5.target.parentNode.parentNode;
+
+    const taskTitle = parentNode.childNodes[3].childNodes[3];
+    const taskDescription = parentNode.childNodes[3].childNodes[5];
+    const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    const submitButton = parentNode.childNodes[5].childNodes[1];
+
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML
+    }
+    let stateCopy = state.taskList;
+
+    stateCopy = stateCopy.map((task) => 
+    task.id === targetId
+        ? {
+            id: task.id,
+            title: updateData.taskTitle,
+            description: updateData.taskDescription,
+            type: updateData.taskType,
+            url: task.url
+        }
+        : task
+    )
+    state.taskList = stateCopy;
+    updateLocalStorage();
+
+    taskTitle.setAttribute("contentEditable", "false");
+    taskDescription.setAttribute("contentEditable", "false");
+    taskType.setAttribute("contentEditable", "false");
+
+    submitButton.setAttribute("onclick", "openTask.apply(this, arguments)");
+    submitButton.setAttribute("data-bs-toggle", "modal");
+    submitButton.setAttribute("data-bs-target", "#showTask");
+    submitButton.innerHTML = "Open Task";
+}
+
+// JS code for the search bar
+const searchTask = (event6) => {
+    if (!event6) {
+        event6 = window.event;
+    }
+    while (taskContents.firstChild) {
+        taskContents.removeChild(taskContents.firstChild);
+    }
+    const resultData = state.taskList.filter(({title}) => 
+        title.toLowerCase().includes(event6.target.value.toLowerCase())
+    )
+
+    resultData.map (
+        (cardDate) => taskContents.insertAdjacentElement("beforeend", htmlTaskContent(cardDate))
+    )
+}
+
+// Continue watching from Day-36 5:28
